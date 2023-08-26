9c74d06d640692cb8df0213ce52504707d70060e
rohaan4 <rohaanshah37@gmail.com>
2023-08-26 21:37:54 +0400

    input of files
    created the third and fourth frame with 2 buttons in each frame, 3rd frame responsible for opening and saving an image, 4th frame for encoding and decoding the data on the image

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import tkinter as tk
 from PIL import Image, ImageTk#importing Image and ImageTk to improve capabilities of image work
 import os
 from stegano import lsb
+
+
 #SETTING UP THE GUI
 root=Tk()#creating main window for application
 root.title("Steganography- Hide your data")#setting title of main window
@@ -14,26 +16,47 @@ root.configure(bg="#2f4155")#setting background color of window
 
 
 #icon of the window
-image_icon=PhotoImage(file="logo_main.png")
-root.iconphoto(False, image_icon)
+image_icon=PhotoImage(file="logo_main.png")#loading an image using photoimage library
+root.iconphoto(False, image_icon)#setting the window icon to the image loaded above
 
 #logo of the window
-logo=PhotoImage(file="logo.png")
-Label(root, image=logo, bg="#2f4155").place(x=10,y=0)
+logo=PhotoImage(file="logo.png")#loading an image for the application logo
+Label(root, image=logo, bg="#2f4155").place(x=10,y=0)#creating label in main window and displaying the logo at (10,0) coordinates
 
 #setting up the heading of the window
 Label(root, text="CYBERSECURITY", bg="#2d4155", fg="white", font="arial 25 bold").place(x=100, y=20)
 
 #first frame
-f=Frame(root, bd=3, bg="black", width=340, height=280, relief=GROOVE)
-f.place(x=10, y=80)
-lbl=Label(f,bg="black")
-lbl.place(x=40,y=10)
+f=Frame(root, bd=3, bg="black", width=340, height=280, relief=GROOVE)#setting up the left side framespecified with border, bg color, dimensions and groove styled border
+f.place(x=10, y=80)#placing it at the coordinates(10,80)
+lbl=Label(f,bg="black")#setting up a label inside and settingbg color to black
+lbl.place(x=40,y=10)#placing the label at (40,10) coordinates in the frame
 
 #second Frame
-frame2=Frame(root, bd=3, width=340, height=280,bg="white", relief=GROOVE)
-frame2.place(x=350, y=80)
-text1=Text(frame2, font="robote 20", bg="white", fg="black", relief=GROOVE)
-text1.place(x=0, y=0, width=320, height=295)
+frame2=Frame(root, bd=3, width=340, height=280,bg="white", relief=GROOVE)#creating the right side frame much like the left(bg is WHITE)
+frame2.place(x=350, y=80)#placing it at (350, 80) coordinates
+text1=Text(frame2, font="robote 20", bg="white", fg="black", relief=GROOVE)#creating text widget for multi-line input
+text1.place(x=0, y=0, width=320, height=295)#placing the text widget in the right side frame
 
+scrollbar1=Scrollbar(frame2)
+scrollbar1.place(x=320, y=0, height=300)
+
+scrollbar1.configure(command=text1.yview)
+text1.configure(yscrollcommand=scrollbar1.set)
+
+#third Frame
+frame3=Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)#creating third frame simliar to the above 2 but with diff. dimensions
+frame3.place(x=10,y=370)#placing it at (10, 370) coordinates
+
+Button(frame3, text="Open Image", width=10, height=2, font="ariel 14 bold").place(x=20, y=30)#making a button in the third frame to open the image
+Button(frame3, text="Save Image", width=10, height=2, font="ariel 14 bold").place(x=180, y=30)#making another button to save the image
+Label(frame3, text="Picture, Image, Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)#creating label above the two buttons to suggest user on input format
+
+#fourth Frame
+frame4=Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)
+frame4.place(x=360,y=370)
+
+Button(frame4, text="Encode Data", width=10, height=2, font="ariel 14 bold").place(x=20, y=30)#creating button in the fourth frame to encode data in image
+Button(frame4, text="Decode Data", width=10, height=2, font="ariel 14 bold").place(x=180, y=30)#creating another button to decode data in the image
+Label(frame4, text="Picture, Image, Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)
 root.mainloop()#starting main loop of window to keep it running
