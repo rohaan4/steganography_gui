df1cbe70e447797c65f85c8780b83a36f7518cbe
rohaan4 <rohaanshah37@gmail.com>
2023-08-26 23:59:35 +0400

    created functionality of the app
    created functions to open image, save image, hide message, show message, show image

--- main.py
+++ main.py
@@ -6,6 +6,86 @@ from PIL import Image, ImageTk#importing Image and ImageTk to improve capabiliti
 import os
 from stegano import lsb
 
+def showimage():
+    global filename
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(),
+                                        title="Select Image File",
+                                        filetypes=(("PNG File", "*.png"),
+                                                  ("JPG File", "*.jpg"),
+                                                  ("All File", "*.*")))
+    img=Image.open(filename)
+    img=ImageTk.PhotoImage(img)
+    lbl.configure(image=img,width=250, height=250)
+    lbl.image=img
+
+from PIL import Image
+
+def message_to_bin(message):
+    """Convert a string message to binary."""
+    binary_message = ''.join(format(ord(i), '08b') for i in message)
+    return binary_message
+
+def bin_to_message(binary_message):
+    """Convert a binary string to its ASCII representation."""
+    ascii_string = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
+    return ascii_string
+
+def hide_message(image_path, message):
+    """Hide the message in the image."""
+    img = Image.open(image_path)
+    binary_message = message_to_bin(message) + '1111111111111110'  # 16 bit terminator
+    pixels = list(img.getdata())
+    new_pixels = []
+    
+    message_index = 0
+    for pixel in pixels:
+        if message_index < len(binary_message):
+            new_pixel = (pixel[0] & ~1 | int(binary_message[message_index]),) + pixel[1:]
+            new_pixels.append(new_pixel)
+            message_index += 1
+        else:
+            new_pixels.append(pixel)
+            
+    img.putdata(new_pixels)
+    img.save('hidden.png')
+    return img
+
+def show_message(image_path):
+    """Reveal the message from the image."""
+    img = Image.open(image_path)
+    pixels = list(img.getdata())
+    
+    binary_message = ''
+    for pixel in pixels:
+        binary_message += str(pixel[0] & 1)
+        
+    terminator = binary_message.find('1111111111111110')
+    if terminator != -1:
+        binary_message = binary_message[:terminator]
+        
+    message = bin_to_message(binary_message)
+    return message
+
+
+
+def hide():
+    global filename
+    message = text1.get(1.0, END)
+    hide_message(filename, message)
+
+
+def show():
+    revealed_message = show_message('hidden.png')
+    text1.delete(1.0, END)
+    text1.insert(END, revealed_message)
+
+
+
+def save():
+    filepath = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("All files", "*.*")])
+    if filepath:
+        img = Image.open("hidden.png")
+        img.save(filepath)
 
 #SETTING UP THE GUI
 root=Tk()#creating main window for application
@@ -48,15 +128,15 @@ text1.configure(yscrollcommand=scrollbar1.set)
 frame3=Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)#creating third frame simliar to the above 2 but with diff. dimensions
 frame3.place(x=10,y=370)#placing it at (10, 370) coordinates
 
-Button(frame3, text="Open Image", width=10, height=2, font="ariel 14 bold").place(x=20, y=30)#making a button in the third frame to open the image
-Button(frame3, text="Save Image", width=10, height=2, font="ariel 14 bold").place(x=180, y=30)#making another button to save the image
+Button(frame3, text="Open Image", width=10, height=2, font="ariel 14 bold",command=showimage).place(x=20, y=30)#making a button in the third frame to open the image
+Button(frame3, text="Save Image", width=10, height=2, font="ariel 14 bold",command=save).place(x=180, y=30)#making another button to save the image
 Label(frame3, text="Picture, Image, Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)#creating label above the two buttons to suggest user on input format
 
 #fourth Frame
 frame4=Frame(root, bd=3, bg="#2f4155", width=330, height=100, relief=GROOVE)
 frame4.place(x=360,y=370)
 
-Button(frame4, text="Encode Data", width=10, height=2, font="ariel 14 bold").place(x=20, y=30)#creating button in the fourth frame to encode data in image
-Button(frame4, text="Decode Data", width=10, height=2, font="ariel 14 bold").place(x=180, y=30)#creating another button to decode data in the image
+Button(frame4, text="Encode Data", width=10, height=2, font="ariel 14 bold",command=hide).place(x=20, y=30)#creating button in the fourth frame to encode data in image
+Button(frame4, text="Decode Data", width=10, height=2, font="ariel 14 bold",command=show).place(x=180, y=30)#creating another button to decode data in the image
 Label(frame4, text="Picture, Image, Photo File", bg="#2f4155", fg="yellow").place(x=20, y=5)
 root.mainloop()#starting main loop of window to keep it running
